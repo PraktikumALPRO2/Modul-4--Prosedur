5e48be843c03ffcdbe7b9fc0dea36cfd4842214a
Mohammad Alfan Naraya Revta <162522372+NarayaAlfan19@users.noreply.github.com>
2024-10-20 20:22:24 +0700

    Add files via upload

---
+++ 2311102170_Mohammad Alfan Naraya/Modul 4/guided2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Prosedur untuk menghitung dan mencetak luas persegi
+func hitungLuas(sisi float64) {
+	luas := sisi * sisi
+	fmt.Printf("Luas persegi: %.2f\n", luas)
+}
+
+// Prosedur untuk menghitung dan mencetak keliling persegi
+func hitungKeliling(sisi float64) {
+	keliling := 4 * sisi
+	fmt.Printf("Keliling persegi: %.2f\n", keliling)
+}
+
+func main() {
+	var sisi float64
+
+	fmt.Print("Masukkan panjang sisi persegi: ")
+	fmt.Scan(&sisi)
+
+	hitungLuas(sisi)
+	hitungKeliling(sisi)
+}
